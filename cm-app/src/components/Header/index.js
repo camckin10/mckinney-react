d3de93415d8bd0913fbe822ac23397ddda42c64b
Chelsey McKinney <chelseymckinney@gmail.com>
2019-01-30 20:31:52 -0600

    renaming files and code for header

---
+++ cm-app/src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Header extends React.Component{
+		render() {
+			return (
+				<div className="header">
+					<h1>This is a test</h1>
+				  <h2>I wanted to see if this appears.</h2>
+				</div>
+				
+			);
+		}
+}
+
+
+
+export default Header;
