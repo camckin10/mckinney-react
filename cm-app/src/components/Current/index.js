bdf6023f18ad5d7e63974a8f7e64c40ba3017664
Chelsey McKinney <chelseymckinney@gmail.com>
2019-01-30 21:17:45 -0600

    still testing countdown timer

--- cm-app/src/components/Current/index.js
+++ cm-app/src/components/Current/index.js
@@ -1,25 +1,62 @@
 import React, {Component} from 'react';
-import {Container, Row, Col} from 'reactstrap';
+//import {Container} from 'reactstrap';
 //import PropTypes from 'prop-types';
-import moment from 'moment';
-import CountdownTimer from 'react-awesome-countdowntimer';
+//import moment from 'moment';
+//import CountdownTimer from 'react-awesome-countdowntimer';
 
 
 
 class Current extends Component{
 	render () {
+// 		componentWillMount() {
+// 			var target_date = new Date().getTime() + (1000*3600*48); // set the countdown date
+// var days, hours, minutes, seconds; // variables for time units
+
+// var countdown = document.getElementById("tiles"); // get tag element
+
+// getCountdown();
+
+// setInterval(function () { getCountdown(); }, 1000);
+
+// function getCountdown(){
+
+// 	// find the amount of "seconds" between now and target
+// 	var current_date = new Date().getTime();
+// 	var seconds_left = (target_date - current_date) / 1000;
+
+// 	days = pad( parseInt(seconds_left / 86400) );
+// 	seconds_left = seconds_left % 86400;
+		 
+// 	hours = pad( parseInt(seconds_left / 3600) );
+// 	seconds_left = seconds_left % 3600;
+		  
+// 	minutes = pad( parseInt(seconds_left / 60) );
+// 	seconds = pad( parseInt( seconds_left % 60 ) );
+
+// 	// format countdown string + set tag value
+// 	countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>"; 
+// }
+
+// function pad(n) {
+// 	return (n < 10 ? '0' : '') + n;
+// }
+
+// 		}
 		return(
-				<Container>
-					<h3>This is my current stuff </h3>
-					<Row>
+				<div className="timer">
 						{/*Adding countdown timer*/}
-						<Col md="6">
-						<CountdownTimer endDate={moment('01/30/2018 08:44:00', 'DD/MM/YYYY hh:mm:ss')}/>
-						</Col>
-						<Col md="6">Twitter Feed placed here.</Col>
-					</Row>
-					<hr/>
-				</Container>
+						<h3>This is my current stuff </h3>
+						<div id="countdown">
+							<div id='tiles'></div>
+								<div className="labels">
+									<li>Days</li>
+									<li>Hours</li>
+									<li>Mins</li>
+									<li>Secs</li>
+								</div>
+						</div>
+				</div>
+
 		)
 	}
 }
